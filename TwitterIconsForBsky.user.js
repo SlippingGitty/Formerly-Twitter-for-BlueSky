b83c24e0365870de3879d83fa663930d8739d3ce
Vozer <76500838+SlippingGitty@users.noreply.github.com>
2024-11-28 21:25:01 -0600

    Create TwitterIconsForBsky.user.js

---
+++ TwitterIconsForBsky.user.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name        Twitter icons for Bsky
+// @description Assortment of Twitter favicons for BlueSky
+// @match       https://bsky.app/*
+// @grant       GM_registerMenuCommand
+// @grant       GM_setValue
+// @grant       GM_getValue
+// @version     0.1
+// @author      vozercozer
+// ==/UserScript==
+
+// i never make userscripts, lemme know if i did this right. idk shit about js lol. 
+(() => {
+  'use strict';
+  const favicons = {
+    Twitter: 'https://files.catbox.moe/52bjpd.ico',
+    'Twitter Classic': 'https://files.catbox.moe/dhlzoa.ico',
+    X: 'https://files.catbox.moe/pkakac.ico',
+  };
+  const updateFavicon = (key) => {
+    document.head.querySelectorAll("link[rel*='icon']").forEach(link => link.remove());
+    document.head.appendChild(Object.assign(document.createElement('link'), {
+      rel: 'icon', href: favicons[key]
+    }));
+  };
+  Object.entries(favicons).forEach(([key]) =>
+    GM_registerMenuCommand(`Set Favicon: ${key}`, () => {
+      GM_setValue('selectedFavicon', key);
+      updateFavicon(key);
+      alert(`Favicon changed to: ${key}`);
+    })
+  );
+  updateFavicon(GM_getValue('selectedFavicon', 'Twitter'));
+})();
